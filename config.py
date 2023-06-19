234a875b5ab6111bd2c56156b5ca88bd58b4b78c
sajadayad <83421612+sajadayad@users.noreply.github.com>
2023-06-19 10:13:30 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-token = '6209900686:AAE0euFE8mBAe1jXIZETcqkcYiAfjicSBdM'
-sudo = 6186573613
+token = '5818590819:AAEOLYMC3X8XR3WS6i9YSkkOmOCJAjpvD3U'
+sudo = '6186573613'
 apikey = '26c08efd-43fa-402a-babb-8ec5652e6a7f'
 appname = 'sajadayadtiktokbot'
 time = 1
