ce3d73a3ac99548be791ac8b185d0a5351abc336
sajadayad <83421612+sajadayad@users.noreply.github.com>
2025-06-09 22:49:48 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '7810117102:AAGVQIUCV4pwwLfZrLPKXzJDNzUb__mlMTw'
+token = '7695142953:AAFYJ1WVVXXfh_ybYE_VUwIhMic0lk7pCxg'
 sudo = 1050391
 apikey = '26c08efd-43fa-402a-babb-8ec5652e6a7f'
 appname = 'sajadayadtiktokbot'
